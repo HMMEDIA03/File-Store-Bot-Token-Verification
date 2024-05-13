4533356da3785d39dda141876b1ccfd5e2ce2307
HMMEDIA03 <malaiya9340@gmail.com>
2024-05-13 12:20:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,8 +27,8 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Shortner (token system) 
 
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shrinkforearn.in")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "api.896974bfeae350cf13872c6b1e1e220c1e8d7189")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "publicearn.com")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "b323441bc55ef14be63018992134d393a62a9da9")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_Download_7x/32")
