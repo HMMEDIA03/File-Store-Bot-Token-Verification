6c9c653964d9cc086ac809620388c99a22282381
HMMEDIA03 <malaiya9340@gmail.com>
2024-05-13 13:24:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "shrinkforearn.in")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "896974bfeae350cf13872c6b1e1e220c1e8d7189")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-IS_VERIFY = os.environ.get("IS_VERIFY", "True")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 TUT_VID = os.environ.get("TUT_VID","https://youtube.com/shorts/drCJLujy8R0?si=i2QPmXcoUEACNTT5")
 
 #force sub channel id, if you want enable force sub
